3d14f336e5f5adbe873e815f6c080a6a91277d8e
Alexis Rios <ale5857@gmail.com>
2026-02-25 20:55:46 -0600

    Fix database config for Render SSL

--- NeoVida/settings.py
+++ NeoVida/settings.py
@@ -93,11 +93,11 @@ TEMPLATES = [
 # ======================
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / "db.sqlite3"),
-        conn_max_age=600
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
-
 
 # ======================
 # AUTH / LOGIN
